f44989d666a36257731fd588e8ad4d8f20f90d4e
Kunhee Ha <kunheeha@gmail.com>
2023-04-27 16:26:48 +0100

    lualine colour change

--- lua/lualine/themes/nord.lua
+++ lua/lualine/themes/nord.lua
@@ -3,33 +3,29 @@ local colors = require("nord.colors")
 local nord = {}
 
 nord.normal = {
-	a = { fg = colors.nord1_gui, bg = colors.nord9_gui },
-	b = { fg = colors.nord5_gui, bg = colors.nord2_gui },
-	c = { fg = colors.nord4_gui, bg = colors.nord1_gui },
+	a = { fg = colors.nord1_gui, bg = colors.nord8_gui, gui = "bold" },
+	b = { fg = colors.nord5_gui, bg = colors.nord0_gui },
+	c = { fg = colors.nord4_gui, bg = colors.nord2_gui },
 }
 
 nord.insert = {
-	a = { fg = colors.nord1_gui, bg = colors.nord4_gui },
-	b = { fg = colors.nord6_gui, bg = colors.nord2_gui },
-	y = {  fg = colors.nord5_gui, bg = colors.nord2_gui },
+	a = { fg = colors.nord1_gui, bg = colors.nord4_gui, gui = "bold" },
+	y = {  fg = colors.nord5_gui, bg = colors.nord3_gui },
 }
 
 nord.visual = {
-	a = { fg = colors.nord0_gui, bg = colors.nord7_gui },
-	b = { fg = colors.nord4_gui, bg = colors.nord2_gui },
-	y = {  fg = colors.nord5_gui, bg = colors.nord2_gui },
+	a = { fg = colors.nord0_gui, bg = colors.nord7_gui, gui = "bold" },
+	y = {  fg = colors.nord5_gui, bg = colors.nord3_gui },
 }
 
 nord.replace = {
-	a = { fg = colors.nord0_gui, bg = colors.nord11_gui },
-	b = { fg = colors.nord4_gui, bg = colors.nord2_gui },
-	y = {  fg = colors.nord5_gui, bg = colors.nord2_gui },
+	a = { fg = colors.nord0_gui, bg = colors.nord11_gui, gui = "bold" },
+	y = {  fg = colors.nord5_gui, bg = colors.nord3_gui },
 }
 
 nord.command = {
 	a = { fg = colors.nord0_gui, bg = colors.nord15_gui, gui = "bold" },
-	b = { fg = colors.nord4_gui, bg = colors.nord2_gui },
-	y = {  fg = colors.nord5_gui, bg = colors.nord2_gui },
+	y = {  fg = colors.nord5_gui, bg = colors.nord3_gui },
 }
 
 nord.inactive = {
